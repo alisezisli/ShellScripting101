c0c44edbd65d1c559392462c9c3b821bdd8360cf
Ali Sezişli <ali@alisezisli.com.tr>
2022-01-15 23:56:54 +0300

    Pattern kıyaslaması
    Pattern'a göre kıyaslama örneği eklendi

---
+++ 16-pattern_eslesmesi.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#Konu anlatımı: https://alisezisli.com.tr/shell-script-9-karar-yapilari 
+
+
+#Pattern'lar üzerinden kıyaslama örneği:
+#Bu script, içinde bulunulan ayın kaç günü olduğunu gösterir:
+
+#Command substitution kullanarak, ay değerini alalım
+#date +%m sayesinde, kaçıncı ayda olduğumuzu çift basamaklı bir 
+#sayı olarak öğreniyoruz: 01, 07, 10, 12 gibi:
+
+ay=$(date +%m)
+
+#Şubat ayının özel durumunu belirtelim:
+if [[ ${ay} -eq 2 ]]
+then
+	echo "Şubat ayında 28 gün vardır. Artık yıllarda 29 çeker."
+#30 günlük aylar için bir pattern yazalım:
+elif [[ ${ay} = @(04|06|09|11) ]]
+then
+	echo "Bu ayda 30 gün vardır."
+else
+	echo "Bu ayda 31 gün vardır."
+fi
